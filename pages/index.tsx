480d10f83b84025c41745b673e813a6aac0c18d8
isveny <amineprojet7@gmail.com>
2022-03-24 19:03:23 +0100

    yay init

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,26 +3,82 @@ import React from "react";
 import FadeIn from "react-fade-in";
 const Home: NextPage = () => {
   return (
-    <FadeIn className="flex flex-col justify-center items-center h-screen bg-purple-600/50 backdrop-blur-md select-none space-y-1">
-      <h1 className="text-purple-300 drop-shadow-xl font-bold text-7xl">
-        Next.JS
-      </h1>
-      <p className="text-purple-200 drop-shadow-md text-xl mx-2">
-        Simple NextJS & Tailwind template aims to make it easier for you to task
-        to build your web projects.
-      </p>
-      <a
-        href="https://github.com/amineohn/next-template"
-        rel="noreffer noopener"
-        target="_blank"
-      >
-        <button className="w-60 h-14 bg-purple-200/40 hover:bg-purple-200/30 transition duration-300 backdrop-blur-lg rounded-lg shadow-2xl shadow-purple-400">
-          <div className="flex justify-center items-center">
-            <p className="text-purple-50 text-center text-xl">Get Started</p>
-          </div>
-        </button>
-      </a>
-    </FadeIn>
+    <div className="flex flex-row">
+      <div className="w-28 h-screen bg-gray-200">
+        <div className="flex flex-col justify-start py-4 items-center w-20 h-full bg-gray-300/40 m-auto space-y-6">
+          <button className="px-2 py-2 rounded-xl hover:bg-indigo-400 text-gray-600 hover:text-gray-50 transition">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-7 w-7"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+              />
+            </svg>
+          </button>
+          <button className="px-2 py-2 rounded-xl hover:bg-indigo-400 text-gray-600 hover:text-gray-50 transition">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-7 w-7 "
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 13h6M3 17V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z"
+              />
+            </svg>
+          </button>
+          <button className="px-2 py-2 rounded-xl hover:bg-indigo-400 text-gray-600 hover:text-gray-50 transition">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-7 w-7 "
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4"
+              />
+            </svg>
+          </button>
+          <button className="px-2 py-2 rounded-xl hover:bg-indigo-400 text-gray-600 hover:text-gray-50 transition">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-7 w-7"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+
+      <div className="container m-auto">
+        <p className="animate-pulse text-center text-7xl text-indigo-600">
+          Hello Spider, j'espère que tu vas réussir 😁
+        </p>
+      </div>
+    </div>
   );
 };
 
