e400a1ccf885b267034935328ecce416775e0cdc
isveny <amineprojet7@gmail.com>
2022-03-25 23:36:36 +0100

    feat: add more icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,82 +3,135 @@ import React from "react";
 import FadeIn from "react-fade-in";
 const Home: NextPage = () => {
   return (
-    <div className="flex flex-row">
-      <div className="w-28 h-screen bg-gray-200">
-        <div className="flex flex-col justify-start py-4 items-center w-20 h-full bg-gray-300/40 m-auto space-y-6">
-          <button className="px-2 py-2 rounded-xl hover:bg-indigo-400 text-gray-600 hover:text-gray-50 transition">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-7 w-7"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-              />
-            </svg>
-          </button>
-          <button className="px-2 py-2 rounded-xl hover:bg-indigo-400 text-gray-600 hover:text-gray-50 transition">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-7 w-7 "
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M9 13h6M3 17V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z"
-              />
-            </svg>
-          </button>
-          <button className="px-2 py-2 rounded-xl hover:bg-indigo-400 text-gray-600 hover:text-gray-50 transition">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-7 w-7 "
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4"
-              />
-            </svg>
-          </button>
-          <button className="px-2 py-2 rounded-xl hover:bg-indigo-400 text-gray-600 hover:text-gray-50 transition">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-7 w-7"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-              />
-            </svg>
-          </button>
+    <>
+      <div className="w-full h-20 bg-gray-200 absolute"></div>
+      <div className="flex flex-row">
+        <div className="w-28 h-screen bg-gray-200 z-50">
+          <div className="flex flex-col justify-between py-4 items-center w-20 h-full bg-gray-300/40 m-auto space-y-6">
+            <div className="px-4 space-y-4">
+              <button className="px-2 py-2 rounded-xl hover:bg-indigo-400 text-gray-600 hover:text-gray-50 transition">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-7 w-7"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                  />
+                </svg>
+              </button>
+              <button className="px-2 py-2 rounded-xl hover:bg-indigo-400 text-gray-600 hover:text-gray-50 transition">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-7 w-7 "
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M9 13h6M3 17V7a2 2 0 012-2h6l2 2h6a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2z"
+                  />
+                </svg>
+              </button>
+              <button className="px-2 py-2 rounded-xl hover:bg-indigo-400 text-gray-600 hover:text-gray-50 transition">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-7 w-7 "
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 7v10c0 2.21 3.582 4 8 4s8-1.79 8-4V7M4 7c0 2.21 3.582 4 8 4s8-1.79 8-4M4 7c0-2.21 3.582-4 8-4s8 1.79 8 4m0 5c0 2.21-3.582 4-8 4s-8-1.79-8-4"
+                  />
+                </svg>
+              </button>
+              <button className="px-2 py-2 rounded-xl hover:bg-indigo-400 text-gray-600 hover:text-gray-50 transition">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-7 w-7"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                  />
+                </svg>
+              </button>
+              <button className="px-2 py-2 rounded-xl hover:bg-indigo-400 text-gray-600 hover:text-gray-50 transition">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-7 w-7"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+                  />
+                </svg>
+              </button>
+              <button className="px-2 py-2 rounded-xl hover:bg-indigo-400 text-gray-600 hover:text-gray-50 transition">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-7 w-7"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M9 19l3 3m0 0l3-3m-3 3V10"
+                  />
+                </svg>
+              </button>
+            </div>
+            <button className="px-2 py-2 rounded-xl hover:bg-indigo-400 text-gray-600 hover:text-gray-50 transition bottom-0">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-7 w-7"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13 5l7 7-7 7M5 5l7 7-7 7"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <div className="container m-auto">
+          <p className="animate-pulse text-center text-7xl text-indigo-600">
+            Hello
+          </p>
         </div>
       </div>
-
-      <div className="container m-auto">
-        <p className="animate-pulse text-center text-7xl text-indigo-600">
-          Hello Spider, j'espère que tu vas réussir 😁
-        </p>
-      </div>
-    </div>
+    </>
   );
 };
 
